8c38d8c1a68b2c7daed87f870dee769d5ce50abf
Pranay Pentaparthy <pranaypentaparthy@Pranays-MacBook-Pro.local>
2025-05-26 17:28:47 -0700

    added batch_counter json file and updated kafka consumer file

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -3,21 +3,35 @@ from pyspark.sql.functions import col
 import time
 import os
 
-# Global batch counter
-batch_id_accumulator = {"count": 0}
+def load_counter(counter_file):
+    if os.path.exists(counter_file):
+        with open(counter_file, 'r') as f:
+            data = json.load(f)
+            return data
+            return data.get("count", 0)
+    return 0
+
+def save_counter(count):
+    with open(counter_file, 'w') as f:
+        json.dump({"count": count}, f)
 
 def custom_write_to_csv(batch_df, batch_id):
     # Increment batch counter
-    batch_id_accumulator["count"] += 1
-    file_name = f"file_{batch_id_accumulator['count']}.csv"
+    batch_file = load_counter(counter_file)
+    batch_file["count"] += 1
+    save_counter(batch_file["count"])
+    file_name = f"file_{batch_file['count']}.csv"
     output_path = os.path.join(output_dir, file_name)
 
     # Write DataFrame to single CSV file (you can repartition to 1 if needed)
     batch_df.coalesce(1).write.mode("overwrite").option("header", True).csv(output_path)
 
-def start_spark_streaming_app(bootstrap_servers, topic_name, output_dir_path):
+def start_spark_streaming_app(bootstrap_servers, 
+    topic_name, output_dir_path, counter_json_file):
     global output_dir
+    global counter_file
     output_dir = output_dir_path  # So custom_write_to_csv can access it
+    counter_file = counter_json_file
 
     spark = SparkSession.builder \
         .appName("KafkaToCSVStreaming") \
@@ -47,6 +61,8 @@ if __name__ == "__main__":
     bootstrap_servers = "kafka-1:9092,kafka-2:9093,kafka-3:9094"
     topic_name = "second_test"
     output_dir = "./messages"
+    counter_file = "batch_counter.json"
 
     time.sleep(5)
-    start_spark_streaming_app(bootstrap_servers, topic_name, output_dir)
+    start_spark_streaming_app(bootstrap_servers, 
+        topic_name, output_dir, counter_file)
